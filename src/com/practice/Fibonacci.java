a7bc9104d7f9fa763c6c05bcbf813c435a90f41c
Gdpatil98 <ganeshkdhawale2705@gmail.com>
2023-11-09 23:30:46 +0530

    first commit

---
+++ src/com/practice/Fibonacci.java
@@ -0,0 +1,22 @@
+package com.practice;
+
+public class Fibonacci {
+	
+	public static void main(String[] args) {
+		
+		
+		int n=10,a=0,b=1;
+		System.out.println("Fibonacci series");
+		
+		for(int i=0;i<=n;++i)	
+		{
+			System.out.println(a+", ");
+		int c=a+b;
+		a=b;
+		b=c;
+		
+		}
+		
+	}
+
+}
